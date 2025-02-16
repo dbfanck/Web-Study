d91b226ad89e3922a864267520bdc0cce3637494
kim <clgus08@gmail.com>
2025-02-17 00:49:58 +0900

    Revert "ch10"
    This reverts commit c232b304bcbbfdb8a08590b4f5c154fc31f3993e.

--- dahyeon/10_1.js
+++
@@ -1,6 +0,0 @@
-function num1(n){
-    var result = n * n * 3.14
-    console.log(result)
-}
-
-num1(5)
--- dahyeon/10_3.js
+++
@@ -1,8 +0,0 @@
-function num3(height, weight){
-    var bmi = height * height / weight
-    if(bmi >= 26) return "비만"
-    else if(bmi >= 24 && bmi < 25) return "과체중"
-    else if(bmi >= 18.5 && bmi <23) return "정상"
-    else if(bmi < 18.5) return "저체중"
-}
-num3(1.8, 70)
